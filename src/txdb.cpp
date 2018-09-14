7ba3b10249b8d90aac737b693f64f5e99852efc5
AitorGonzalezMartínez <a.gonzalez.coin@gmail.com>
2018-09-14 07:48:56 +0200

    Fixing txdb.cpp

--- src/txdb.cpp
+++ src/txdb.cpp
@@ -272,7 +272,7 @@ bool CBlockTreeDB::LoadBlockIndexGuts()
 
                 //populate accumulator checksum map in memory
                 if(pindexNew->nAccumulatorCheckpoint != 0 && pindexNew->nAccumulatorCheckpoint != nPreviousCheckpoint) {
-                    /LoadAccumulatorValuesFromDB(pindexNew->nAccumulatorCheckpoint);
+                    LoadAccumulatorValuesFromDB(pindexNew->nAccumulatorCheckpoint);
                     nPreviousCheckpoint = pindexNew->nAccumulatorCheckpoint;
                 }
 
