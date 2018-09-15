b507143996cbbc77a4a93e73f19116788703a20b
AitorGonzalezMartínez <a.gonzalez.coin@gmail.com>
2018-09-15 23:48:02 +0200

    travis builder yml

--- build.sh
+++ build.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
- set -e
- ./autogen.sh
+
+set -e
+
+./autogen.sh
 cd depends && make $MAKEJOBS $OPT
 cd $TRAVIS_BUILD_DIR && ./configure --prefix=$TRAVIS_BUILD_DIR/depends/$HOST $CONF
 make $MAKEJOBS $GOAL
