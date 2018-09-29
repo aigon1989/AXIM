120e98ced1e3b35e88448d94bd7d0b1b551c162e
Aitor González Martínez <a.gonzalez.coin@gmail.com>
2018-09-29 18:22:23 +0200

    Temporarely disabling time block fork check

--- src/main.cpp
+++ src/main.cpp
@@ -1852,8 +1852,9 @@ bool IsInitialBlockDownload()
     static bool lockIBDState = false;
     if (lockIBDState)
         return false;
-    bool state = (chainActive.Height() < pindexBestHeader->nHeight - 24 * 6 ||
-                  pindexBestHeader->GetBlockTime() < GetTime() - 6 * 60 * 60); // ~144 blocks behind -> 2 x fork detection time
+    //bool state = (chainActive.Height() < pindexBestHeader->nHeight - 24 * 6 ||
+    //              pindexBestHeader->GetBlockTime() < GetTime() - 6 * 60 * 60); // ~144 blocks behind -> 2 x fork detection time
+    bool state = true;
     if (!state)
         lockIBDState = true;
     return state;
